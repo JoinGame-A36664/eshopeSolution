d621ff7463f6368e46ba4e1b4b507bd46b5b03ca
a36664 Nguyễn Văn Lực <a36664@thanglong.edu.vn>
2020-07-28 23:42:21 +0700

    Create login and register

---
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -0,0 +1,86 @@
+﻿using eShopSolution.Data.Entities;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.System.Users
+{
+    public class UserService : IUserService
+    {
+        //UserManager và SignInManager nằm trong thư viện using Microsoft.AspNetCore.Identity;
+        // phải config bên Startup của project Api nha như kiểu Di ý
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<AppRole> _roleManager;
+        private readonly IConfiguration _config;
+
+        public UserService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<AppRole> roleManager, IConfiguration config)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _config = config;
+        }
+
+        public async Task<string> Authencate(LoginRequest request) // xác thực đăng nhập
+        {
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            if (user == null)
+                return null;
+
+            var result = await _signInManager.PasswordSignInAsync(user, request.Password, request.RememberMe, true); // bằng true là cùa lockoutOnfilure là khi login nhiều quá ta sẽ khóa tài khoản lại
+            if (!result.Succeeded)// NẾU đăng nhập thành công
+            {
+                return null;
+            }
+            var roles = await _userManager.GetRolesAsync(user); // lấy một list role của user
+            var claims = new[] //claims yêu cầu trả lại
+            {
+                new Claim(ClaimTypes.GivenName,user.FirstName),
+                 new Claim(ClaimTypes.Email,user.Email),
+                 new Claim(ClaimTypes.Role,string.Join(";",roles)) // string.Join là nối tất cả các dối dượng lại thành một chuối cách nhau bới giấu ;
+            };
+
+            // bắt đầu mã hóa Claim bằng Symmetric vào appseting của Api cài đặt
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Tokens:Issuer"],
+                _config["Tokens:Issuer"],
+                claims,
+                expires: DateTime.Now.AddHours(3),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        // đăng kí user
+        public async Task<bool> Register(RegisterRequest request)
+        {
+            var user = new AppUser()
+            {
+                DOB = request.DOB,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                UserName = request.UserName,
+                PhoneNumber = request.PhoneNumber
+            };
+
+            var rusult = await _userManager.CreateAsync(user, request.Password);
+            if (rusult.Succeeded)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ eShopSolution.BackendApi/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShopSolution.Application.System.Users;
+using eShopSolution.ViewModels.System.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShopSolution.BackendApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // bắt đâu tiêm Di nhớ đăng kí bên startup của APi nhe
+
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // BẮT ĐẦU THỰC HIỆN CÁC PHƯƠNG THỨC CỦA USER
+
+        [HttpPost("authenticate")]
+        [AllowAnonymous] // chưa đăng nhập vẫn gọi được cái này
+        public async Task<IActionResult> Authencate([FromForm] LoginRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var resultToken = await _userService.Authencate(request);
+            if (string.IsNullOrEmpty(resultToken))
+            {
+                return BadRequest("Username or password is incorrect");
+            }
+
+            return Ok(new { token = resultToken });
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous] // chưa đăng nhập vẫn gọi được cái này
+        public async Task<IActionResult> Register([FromForm] RegisterRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _userService.Register(request);
+            if (!result)
+            {
+                return BadRequest("Regisster is unsuccessful");
+            }
+
+            return Ok();
+        }
+    }
+}
